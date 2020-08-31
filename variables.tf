a4622b2fd7d7181f5fdd1c2c199ddfd7abab7115
Daniel Rodriguez <picta.ricta@gmail.com>
2020-08-31 18:28:32 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,14 +19,3 @@ variable "storage_account_replication_type" {
   description = "(Required) Defines the type of replication to use for this storage account. Valid options are LRS*, GRS, RAGRS and ZRS."
   default     = "LRS"
 }
-
-## A network_rules block 
-variable "default_action" {
-  description = "(Required) Specifies the default action of allow or deny when no other rules match. Valid options are Deny or Allow"
-  default = "Deny"
-}
-
-variable "virtual_network_subnet_ids" {
-  description = "(Required) A list of resource ids for subnets."
-  type = "list"
-}
