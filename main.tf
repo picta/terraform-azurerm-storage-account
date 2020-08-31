9f2d23fa392037c1e97e683476fc568677cb6b86
Daniel Rodriguez <picta.ricta@gmail.com>
2020-08-31 18:44:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,4 +13,4 @@ resource "azurerm_storage_account" "storage_account" {
   account_tier             = "${var.storage_account_tier}"
   resource_group_name      = "${var.resource_group_name}"
   account_replication_type = "${var.storage_account_replication_type}"
-  
+}
