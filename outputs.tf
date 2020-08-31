6412ddb187b187090e0b36cc0dd859d4af88963a
Daniel Rodriguez <picta.ricta@gmail.com>
2020-08-31 18:23:49 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "storage_account_name" {
-  value = "${azurerm_storage_account.sa.name}"
+  value = "${azurerm_storage_account.storage_account.name}"
 }
 
 output "storage_account_id" {
-  value = "${azurerm_storage_account.sa.id}"
+  value = "${azurerm_storage_account.storage_account.id}"
 }
 
 output "storage_account_primary_blob_endpoint" {
-  value = "${azurerm_storage_account.sa.primary_blob_endpoint}"
+  value = "${azurerm_storage_account.storage_account.primary_blob_endpoint}"
 }
